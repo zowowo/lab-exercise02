236c2593dd390fd0c19c1c81c8858768972c185e
Breadrool <128883713+Breadrool@users.noreply.github.com>
2023-04-01 15:55:46 +0800

    more  javascript
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
     - addrow
 - delete row */
 
+
+document.getElementById("formtbl").addEventListener("btn-submit", function (event) { event.preventDefault(); submitForm(); });
+
 function addNewRow() {
     var maintable = table.insertRow(-1);
 
@@ -23,5 +26,19 @@ function deleteRow() {
     var table = document.getElementById("tbl_prof")
     var rowIndex = row.parentNode.parentNode.rowIndex;
     table.deleteRow(rowIndex);
+}
+
+function allLetter(inputtxt) {
+    var letters = /^[a-z]*$/i;
+    if (!inputtxt.value.match(letters)) {
+        alert('Please input letters only');
+    }
+}
+
+function submitForm() {
+    let name = document.getElementById("name").value;
+    let course = document.getElementById("course_slct").value;
+    let role = document.getElementById("role_slct").value;
+    window.location.href = "index2.html?name=" + name + "&course_slct=" + course + "&role_slct" + role;
 }
 
