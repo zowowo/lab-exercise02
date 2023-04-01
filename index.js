c84554744072a7c533febc15b78bae061e245031
zowowo <zoepineda47@gmail.com>
2023-04-01 15:49:29 +0800

    first js commit (not functional yet)
    kakayanin

--- index.js
+++ index.js
@@ -3,9 +3,25 @@
 - delete row */
 
 function addNewRow() {
+    var maintable = table.insertRow(-1);
 
+    var picture = document.getElementbyId("file");
+    var name = document.getElementById("name");
+    var role = document.getElementById("role_slct");
+    var course = document.getElementById("course_slct");
+
+    var row = {};
+    var myTable = document.querySelector("table#tableProducts>tbody");
+    row = myTable.insertRow();
+    row.insertCell(-1).textContent = "dummy";
+    var lastRow = row;
+    var lastCell = lastRow.cells[lastRow.cells.length - 1];
+    lastCell.innerHTML = "<button id='btnEdit';>Editar</button>";
 }
 
 function deleteRow() {
-
+    var table = document.getElementById("tbl_prof")
+    var rowIndex = row.parentNode.parentNode.rowIndex;
+    table.deleteRow(rowIndex);
 }
+
